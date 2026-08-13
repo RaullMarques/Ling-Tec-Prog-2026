d3e12c6e81745d0062bc7ce6037b6b267d59ae9e
RaullMarques <raulmarques322@gmail.com>
2026-08-12 22:28:06 -0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+
+	int main(int argc, char *argv[]) {
+	
+	int primeiro, segundo, aux;
+	
+	printf("Digite o primeiro Valor:");
+	scanf("%d", &primeiro);
+	printf("Digite o degundo Valor:");
+	scanf("%d", &segundo);
+	
+	aux = primeiro;
+	primeiro = segundo;
+	segundo = aux;
+	
+	printf("%d \n %d", primeiro, segundo);
+	
+	
+	return 0;
+}
