113c147b2f485ea1cf0d08b8d6ac4b8e5af4eed1
Raul Marques <raulmarques322@gmail.com>
2026-08-23 17:28:24 -0300

    Add speed conversion program from km/h to m/s
    Implement a program to convert speed from km/h to m/s.

---
+++ lista 2. ex 2 .C
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main(int argc, char *argv[]) {
+	
+	float k, m;
+	
+	scanf("%f", &k);
+	
+	m = k / 3.6;
+	
+	printf("%.2f\n", m);
+			
+	return 0;
+}
