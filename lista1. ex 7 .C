cb24e43d7dc72fcb6dea526024c93e129d452d7a
RaullMarques <raulmarques322@gmail.com>
2026-08-23 16:37:08 -0300

    Implement volume calculation for a sphere
    Fix volume calculation formula and add missing newline.

---
+++ lista1. ex 7 .C
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main(int argc, char *argv[]) {
+	
+	double raio, volume;
+	double pi = 3.14159;
+	
+	scanf("%lf", &raio);
+	
+	volume = (4.0 / 3.0) * pi * (raio * raio15 * raio);
+	
+	printf("VOLUME = %.3lf\n",volume);
+			
+	return 0;
+}
